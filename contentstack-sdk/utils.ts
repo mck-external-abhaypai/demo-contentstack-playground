bb8047cfe452802e8dcb71672e78389e2f6e395d
Pavnish Kumar <pavnish.kumar@materialplus.io>
2025-09-11 10:14:05 +0530

    add the preview and visual editor for article

--- contentstack-sdk/utils.ts
+++ contentstack-sdk/utils.ts
@@ -1,7 +1,8 @@
 import { Config, Region, LivePreview, Stack } from "contentstack";
 import getConfig from "next/config";
+
 const { publicRuntimeConfig } = getConfig();
-const envConfig = process.env.CONTENTSTACK_API_KEY
+const envConfig = typeof window === 'undefined' && process.env.CONTENTSTACK_API_KEY
   ? process.env
   : publicRuntimeConfig;
 const {
