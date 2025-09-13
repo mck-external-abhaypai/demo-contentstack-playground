70ff9ba993f797de770935bd942063676ac5992f
Pavnish Kumar <pavnish.kumar@materialplus.io>
2025-09-13 15:32:20 +0530

    CSR

--- config/contentstack/previewSDk.ts
+++ config/contentstack/previewSDk.ts
@@ -18,7 +18,7 @@ ContentstackLivePreview.init({
         locale: getLocaleForURL()
     },
     stackSdk: Stack.config as IStackSdk,
-    ssr: false
+    ssr: true
 })
 
 export const previewSdk = {
